229f9d828d6455062ec35ee03e0c214080c2176a
khurana-maulik <164911947+khurana-maulik@users.noreply.github.com>
2025-06-19 22:35:44 +0530

    analytical update

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
 import Script from 'next/script'
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({ subsets: ["latin"] })
 
